671f4d72a89ea83eec25407929f1d1e46964f12b
ngojohn2002 <ngojohn2002@yahoo.com>
2024-08-26 02:09:07 -0700

    implemented Resume page

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,19 +1,25 @@
-// Objective: Create a Resume component that will be used to display a resume and provide a link to download the resume. The Resume component will be imported and rendered in the App component to display the resume information. The Resume component will contain a link to download the resume file. 
+// Resume.jsx
 
-// Import the React package so that we can use the React library in our component. 
 import React from "react";
 
-// Define the Resume component that will be rendered in the App component. 
 function Resume() {
   return (
     <section>
-      <h2>Resume</h2>
-      <a href="/path/to/resume.pdf" download>
+      <h2>My Resume</h2>
+      <p>
+        Below, you can find a downloadable version of my resume. It highlights
+        my skills, experience, and projects I've worked on. Feel free to
+        download it and learn more about my professional background.
+      </p>
+      <a
+        href="/Truong-Ngo-Full-Stack-Developer-Resume.pdf"
+        download
+        className="download-button"
+      >
         Download Resume
       </a>
     </section>
   );
 }
 
-// Export the Resume component so that it can be imported and used in the App component. 
 export default Resume;
